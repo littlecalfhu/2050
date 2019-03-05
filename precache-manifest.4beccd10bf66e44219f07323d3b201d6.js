ffb5834946f43e617d49656740b3e070cd363bea
huyifan <yf.hu@tianrang-inc.com>
2019-03-05 15:47:00 +0800

    init

---
+++ precache-manifest.4beccd10bf66e44219f07323d3b201d6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "b32bec9aa460db865583",
+    "url": "/static/js/5.b32bec9a.chunk.js"
+  },
+  {
+    "revision": "a7228bf50e8600e83def",
+    "url": "/static/js/main.a7228bf5.chunk.js"
+  },
+  {
+    "revision": "5a9531740b4a5a5b7ea9adb13c46b73c",
+    "url": "/static/media/logo.5a953174.svg"
+  },
+  {
+    "revision": "6444dbe18793a1c7272f",
+    "url": "/static/js/runtime~main.6444dbe1.js"
+  },
+  {
+    "revision": "b2a8973c5299ad8fc66a",
+    "url": "/static/js/8.b2a8973c.chunk.js"
+  },
+  {
+    "revision": "0738cbf07bc0a2c21996",
+    "url": "/static/js/7.0738cbf0.chunk.js"
+  },
+  {
+    "revision": "0f55ffad9244ec1a4c7c",
+    "url": "/static/js/6.0f55ffad.chunk.js"
+  },
+  {
+    "revision": "730d7c2782282ab69b8d",
+    "url": "/static/js/4.730d7c27.chunk.js"
+  },
+  {
+    "revision": "a14ac348d97d381125db",
+    "url": "/static/js/3.a14ac348.chunk.js"
+  },
+  {
+    "revision": "843555c227394ace1391",
+    "url": "/static/js/2.843555c2.chunk.js"
+  },
+  {
+    "revision": "2beb5b327dfacfaee61e05534628b326",
+    "url": "/index.html"
+  }
+];
